04c24d52d55315f3db75410bf83626594fc98b43
DESKTOP-25E5L8O\Ferran <fcarbonellpo@uoc.edu>
2021-03-30 19:47:35 +0200

    Main menu + executable de prova

---
+++ TFM/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Exit();
+        }
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit Game");
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Configure()
+    {
+        Debug.Log("Configure");
+    }
+
+}
