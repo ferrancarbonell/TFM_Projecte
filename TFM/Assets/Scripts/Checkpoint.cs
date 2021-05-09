f68d8cfc50a9a46addba29bf29f7eb132ca01c2e
DESKTOP-25E5L8O\Ferran <fcarbonellpo@uoc.edu>
2021-05-09 21:13:55 +0200

    lightmaps + checkpoint, pause, death sounds

--- TFM/Assets/Scripts/Checkpoint.cs
+++ TFM/Assets/Scripts/Checkpoint.cs
@@ -6,11 +6,13 @@ public class Checkpoint : MonoBehaviour
 {
     public ParticleSystem getCheck;
     private RespawnManager respawnManager;
+    private AudioSource audioSource;
 
     void Start()
     {
         respawnManager = GameObject.FindGameObjectWithTag("Respawn").GetComponent<RespawnManager>();
         getCheck.Stop();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other)
@@ -19,6 +21,7 @@ public class Checkpoint : MonoBehaviour
         {
             respawnManager.lastCheckpointPos = transform.position;
             getCheck.Play();
+            audioSource.Play();
         }
     }
 }
--- TFM/Assets/Scripts/GameManager.cs
+++ TFM/Assets/Scripts/GameManager.cs
@@ -9,12 +9,15 @@ public class GameManager : MonoBehaviour
     public bool isPlayerAlive;
     private RespawnManager respawnManager;
     public GameObject flema;
+    public AudioClip deathAudio;
+    private AudioSource audioSource;
 
     void Start()
     {
         Application.targetFrameRate = 60;
         isPlayerAlive = true;
         respawnManager = GameObject.FindGameObjectWithTag("Respawn").GetComponent<RespawnManager>();
+        audioSource = GetComponent<AudioSource>();
         flema.transform.position = respawnManager.lastCheckpointPos;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -30,6 +33,7 @@ public class GameManager : MonoBehaviour
         isPlayerAlive = false;
         blackFader.GetComponent<Fade>().FadeOut();
         Debug.Log ("Player death");
+        audioSource.PlayOneShot(deathAudio);
         
         yield return new WaitForSeconds(3f);
         //Scene scene = SceneManager.GetActiveScene();
