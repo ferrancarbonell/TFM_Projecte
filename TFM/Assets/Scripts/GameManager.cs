22a900a64b44b503b5414fa8ab853009e2e0ca2f
DESKTOP-25E5L8O\Ferran <fcarbonellpo@uoc.edu>
2021-05-27 18:58:40 +0200

    Configure menú implemented + Helpers update

--- TFM/Assets/Scripts/GameManager.cs
+++ TFM/Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bee2306055f6fa7c616eb1df9f209d341d4570aa9cadb5c9b3c6986dfc78616
-size 1737
+oid sha256:110e07f334cb302d536aed0161c1af5687a6b3534211b8dd4e5f9e61d1624b1d
+size 2060
--- TFM/Assets/Scripts/LevelMusic.cs
+++ TFM/Assets/Scripts/LevelMusic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7836a4f851c9b213f23fddf8e48cd64012b8a1b02c1d39c87feaa27e291af71b
-size 434
+oid sha256:2d5875b9cba1acb6a24f385b1cac8596d9462fb8c3444d93dbd1f599cb976b3b
+size 598
